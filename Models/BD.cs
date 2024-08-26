1ed53c4dd43e41c2197f99bba17bd61f14eeda58
LUCIANA MICAELA DOMINGUEZ SZTERN <48039458@ort>
2024-08-26 10:09:26 -0300

    terminamos clase BD

--- Models/BD.cs
+++ Models/BD.cs
@@ -2,16 +2,31 @@ using System.Data.SqlClient;
 using Dapper;
 
 public class BD{
-    public static List<string> _ListaCategorias; 
+    public static List<Categoria> _ListaCategorias; 
+    public static List<Dificultad> _ListaDificultades;  
+    
 private static string _connectionString = @"Server=localhost;
 DataBase=PreguntadORT;Trusted_Connection=True;";
-    public static List<string> ObtenerCategorias()
+    public static List<Categoria> ObtenerCategorias()
     { 
         using(SqlConnection db = new SqlConnection(_connectionString)){
-            string sql = "SELECT Nombre FROM Categorias";
-            _ListaCategorias = db.Query<string>(sql).ToList();
+            string sql = "SELECT * FROM Categorias";
+            _ListaCategorias = db.Query<Categoria>(sql).ToList();
         }
         
         return _ListaCategorias; 
-    }
+    } 
+    public static List<Dificultad> ObtenerDificultades()
+    { 
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+            string sql = "SELECT * FROM Dificultades";
+            _ListaDificultades = db.Query<Dificultad>(sql).ToList();
+        }
+        
+        return _ListaDificultades; 
+    } 
+    //public static List<Pregunta> ObtenerPreguntas(int dificultad, int categoria) 
+    //{
+
+   // }
 }
