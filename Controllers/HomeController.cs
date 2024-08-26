d9380f95053e099cced9a2e33f36e4f3b735b893
Renata Minuchin <162522630+Ren4ta@users.noreply.github.com>
2024-08-26 11:57:58 -0300

    HomeController casi terminado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,5 +16,47 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         return View();
+    }  
+
+    public  IActionResult ConfigurarJuego() 
+    { 
+       Juego.InicializarJuego();  
+        ViewBag.DatosCategorias = BD.ObtenerCategorias(); 
+        ViewBag.DatosDificultades = BD.ObtenerDificultades(); 
+        return View();
+    }  
+
+    public IActionResult Comenzar(string username, int dificultad, int categoria) 
+    {
+        Juego.CargarPartida(username, dificultad, categoria); 
+        return View("Jugar"); 
+        /* Recibe el username, dificultad y categoría elegidas por el usuario,
+         invoca al método CargarPartida de la clase Juego y, siempre y cuando lleguen preguntas de la base de datos,
+          redirige el sitio al ActionResult Jugar. En caso de elegir una configuración sin preguntas en base de datos, 
+          debe redirigir nuevamente al ActionResult ConfigurarJuego. 
+          A CHECKEAR
+           */ 
+    } 
+
+    public IActionResult Jugar() 
+    {
+        ViewBag.DatosPreguntaActual = Juego.ObtenerProximaPregunta();  
+        if (DatosPreguntaActual.Count == 0 )
+        {
+            return View("Fin")
+        } 
+        else 
+        {
+            ViewBag.DatosRespuestas = Juego.ObtenerProximasRespuestas(int IdPregunta);  
+            return View("Juego")
+        }
+
+    } 
+
+    public [HttpPost] IActionResult VerificarRespuesta(int idPregunta, int idRespuesta) 
+    {
+        ViewBag.Respuesta = Juego.VerificarRespuesta();   
+        return View("Respuesta");
+
     }
 }
